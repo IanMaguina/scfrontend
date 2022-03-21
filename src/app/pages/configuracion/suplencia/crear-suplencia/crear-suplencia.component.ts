78534d4c6fd29cc512f37aa2e1569c7944c0e87c
ian <ianmaguina@gmail.com>
2022-03-21 18:13:28 -0500

    editar documento valorado

--- src/app/pages/configuracion/suplencia/crear-suplencia/crear-suplencia.component.ts
+++ src/app/pages/configuracion/suplencia/crear-suplencia/crear-suplencia.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { Usuario } from 'src/app/models/usuario.interface';
 import { FormValidatorService } from 'src/app/services/form-validator.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
@@ -12,9 +14,11 @@ import { UsuarioService } from 'src/app/services/usuario.service';
   ]
 })
 export class CrearSuplenciaComponent implements OnInit {
+
   
-  listadoUsuarios:Usuario[]=[];
-  crearFormDialog: any;
+  
+  crearFormDialog: FormGroup;
+  
   formErrors = {
     'usuario': '',
     'usuario_suplente': '',
@@ -38,9 +42,16 @@ export class CrearSuplenciaComponent implements OnInit {
   //Submitted form
   submitted = false;
   carga: boolean = false;
+  
+  comboListadoUsuario: Usuario[] = [];
+  filteredUsuario!: Observable<Usuario[]>;
+  selectedUsuario: any;
 
-
-
+  comboListadoUsuarioSuplente: Usuario[] = [];
+  filteredUsuarioSuplente!: Observable<Usuario[]>;
+  selectedUsuarioSuplente: any;
+  
+  
   constructor(
     public dialogRef: MatDialogRef<CrearSuplenciaComponent>,
     private formBuilder: FormBuilder,
@@ -60,18 +71,70 @@ export class CrearSuplenciaComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.listarUsuarios();
+    this.listarUsuariosSuplentes();
   }
 
-  
+
 
   async crearSuplencia(form: any) {
-    
+    console.log("crearSuplencia");
   }
 
-  
+
 
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+  /*  auto complete*/
+  async listarUsuarios() {
+    let listado = await this.usuarioService.listarUsuarios().then();
+    this.comboListadoUsuario = listado;
+    this.filteredUsuario = this.crearFormDialog.get('usuario')?.valueChanges
+    .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.nombre),
+        map(nombre => nombre ? this._filter(nombre) : this.comboListadoUsuario.slice())
+      );
+  }
+
+  displayFn(user: Usuario): string {
+    return user && user.nombre ? user.nombre : '';
+  }
+
+  private _filter(nombre: string): Usuario[] {
+    let filterValue = nombre.toLowerCase();
+    return this.comboListadoUsuario.filter(option => option.nombre.toLowerCase().indexOf(filterValue) === 0);
+  }
+  /*  */
+  /*  auto complete suplente*/
+
+  async listarUsuariosSuplentes() {
+    let listadoSuplentes = await this.usuarioService.listarUsuarios().then();
+    this.comboListadoUsuarioSuplente = listadoSuplentes;
+    this.filteredUsuarioSuplente = this.crearFormDialog.get('usuario_suplente')?.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.nombre),
+        map(nombre => nombre ? this._filterSuplente(nombre) : this.comboListadoUsuario.slice())
+      );
+  }
+
+  displayFnSuplente(user: Usuario): string {
+    return user && user.nombre ? user.nombre : '';
+  }
+
+  private _filterSuplente(nombre: string): Usuario[] {
+    let filterValue = nombre.toLowerCase();
+    return this.comboListadoUsuario.filter(option => option.nombre.toLowerCase().indexOf(filterValue) === 0);
+  }
+  /*  */
+
+
+
+
+
+
 
 }
--- src/app/pages/configuracion/tipoDocumentoValorado/editar-tipo-documento-valorado/editar-tipo-documento-valorado.component.ts
+++ src/app/pages/configuracion/tipoDocumentoValorado/editar-tipo-documento-valorado/editar-tipo-documento-valorado.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormValidatorService } from 'src/app/services/form-validator.service';
 
 @Component({
   selector: 'app-editar-tipo-documento-valorado',
@@ -7,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class EditarTipoDocumentoValoradoComponent implements OnInit {
+  tipoDVData: any;
+  editarFormDialog: any;
+  formErrors = {
+    'nombre': '',
+  }
+  validationMessages = {
+    'nombre': {
+      'required': 'el nombre es requerido.'
+    },
+  };
+  //Submitted form
+  submitted = false;
+  carga: boolean = false;
 
-  constructor() { }
+  //poner el tipado correcto
+
+  constructor(
+    public dialogRef: MatDialogRef<EditarTipoDocumentoValoradoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private formBuilder: FormBuilder,
+    private formValidatorService: FormValidatorService,
+  ) {
+    this.tipoDVData = data;
+    console.log("info del usuario: "+ JSON.stringify(data));
+    this.editarFormDialog = this.formBuilder.group({
+      nombre: [this.tipoDVData.nombre, Validators.required],
+    })
+    this.editarFormDialog.valueChanges.subscribe(() => {
+      this.formErrors = this.formValidatorService.handleFormChanges(this.editarFormDialog, this.formErrors, this.validationMessages, this.submitted);
+    })
+  }
 
   ngOnInit(): void {
+    console.log("");
+  }
+ 
+
+  async editarTipoDocumentoValorado(form: any) {
+    console.log("editarTipoDocumentoValorado");
+
+    this.onNoClick();
+  }
+
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
