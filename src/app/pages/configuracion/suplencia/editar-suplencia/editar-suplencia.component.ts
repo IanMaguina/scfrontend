3dc663896a0d27cbb23de3e901dfe3ea924dd122
ian <ianmaguina@gmail.com>
2022-03-21 19:19:01 -0500

    suplencias

--- src/app/pages/configuracion/suplencia/editar-suplencia/editar-suplencia.component.ts
+++ src/app/pages/configuracion/suplencia/editar-suplencia/editar-suplencia.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { Usuario } from 'src/app/models/usuario.interface';
+import { FormValidatorService } from 'src/app/services/form-validator.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-editar-suplencia',
@@ -8,9 +15,129 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarSuplenciaComponent implements OnInit {
 
-  constructor() { }
+  suplenciaData: any;
+  editarFormDialog: FormGroup;
+  
+  formErrors = {
+    'usuario': '',
+    'usuario_suplente': '',
+    'fecha_inicio': '',
+    'fecha_fin': ''
+  }
+  validationMessages = {
+    'usuario': {
+      'required': 'el usuario es requerido.'
+    },
+    'usuario_suplente': {
+      'required': 'el usuario_suplente es requerido.',
+    },
+    'fecha_inicio': {
+      'required': 'la fecha_inicio es requerida.',
+    },
+    'fecha_fin': {
+      'required': 'el fecha_fin es requerido.',
+    }
+  };
+  //Submitted form
+  submitted = false;
+  carga: boolean = false;
+  
+  comboListadoUsuario: Usuario[] = [];
+  filteredUsuario!: Observable<Usuario[]>;
+  selectedUsuario: any;
+
+  comboListadoUsuarioSuplente: Usuario[] = [];
+  filteredUsuarioSuplente!: Observable<Usuario[]>;
+  selectedUsuarioSuplente: any;
+  
+  
+  constructor(
+    public dialogRef: MatDialogRef<EditarSuplenciaComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private formBuilder: FormBuilder,
+    private formValidatorService: FormValidatorService,
+    private usuarioService: UsuarioService
+  ) {
+    this.suplenciaData = data;
+    this.editarFormDialog = this.formBuilder.group({
+      usuario: [this.suplenciaData.usuario, Validators.required],
+      usuario_suplente: [this.suplenciaData.usuario_suplente, Validators.required],
+      fecha_inicio: [this.suplenciaData.fecha_inicio, Validators.required],
+      fecha_fin: [this.suplenciaData.fecha_fin, Validators.required],
+    })
+    this.editarFormDialog.valueChanges.subscribe(() => {
+      this.formErrors = this.formValidatorService.handleFormChanges(this.editarFormDialog, this.formErrors, this.validationMessages, this.submitted);
+    })
+  }
 
   ngOnInit(): void {
+    this.listarUsuarios();
+    this.listarUsuariosSuplentes();
   }
+
+
+
+  async crearSuplencia(form: any) {
+    console.log("crearSuplencia");
+  }
+
+
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  /*  auto complete*/
+  async listarUsuarios() {
+    let listado = await this.usuarioService.listarUsuarios().then();
+    this.comboListadoUsuario = listado;
+    this.filteredUsuario = this.editarFormDialog.get('usuario')?.valueChanges
+    .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.nombre),
+        map(nombre => nombre ? this._filter(nombre) : this.comboListadoUsuario.slice())
+      );
+  }
+
+  displayFn(user: Usuario): string {
+    return user && user.nombre ? user.nombre : '';
+  }
+
+  private _filter(nombre: string): Usuario[] {
+    let filterValue = nombre.toLowerCase();
+    return this.comboListadoUsuario.filter(option => option.nombre.toLowerCase().indexOf(filterValue) === 0);
+  }
+  /*  */
+  /*  auto complete suplente*/
+
+  async listarUsuariosSuplentes() {
+    let listadoSuplentes = await this.usuarioService.listarUsuarios().then();
+    this.comboListadoUsuarioSuplente = listadoSuplentes;
+    this.filteredUsuarioSuplente = this.editarFormDialog.get('usuario_suplente')?.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.nombre),
+        map(nombre => nombre ? this._filterSuplente(nombre) : this.comboListadoUsuario.slice())
+      );
+  }
+
+  displayFnSuplente(user: Usuario): string {
+    return user && user.nombre ? user.nombre : '';
+  }
+
+  private _filterSuplente(nombre: string): Usuario[] {
+    let filterValue = nombre.toLowerCase();
+    return this.comboListadoUsuario.filter(option => option.nombre.toLowerCase().indexOf(filterValue) === 0);
+  }
+  /*  */
+  async editarSuplencia(form: any) {
+    //call edit suplencia
+    this.onNoClick();
+  }
+
+
+
+
+
 
 }
--- src/app/pages/configuracion/suplencia/suplencia/suplencia.component.ts
+++ src/app/pages/configuracion/suplencia/suplencia/suplencia.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
+import { CrearSuplenciaComponent } from '../crear-suplencia/crear-suplencia.component';
+import { EditarSuplenciaComponent } from '../editar-suplencia/editar-suplencia.component';
 
 @Component({
   selector: 'app-suplencia',
@@ -7,10 +11,80 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SuplenciaComponent implements OnInit {
-
-  constructor() { }
+  listadoSuplencias:any[]=[
+    {id:1, usuario:{ id:1, nombre: 'josé Arantegui' }, usuario_suplente:{ id:3, nombre: 'Nino Bravo' }, fecha_inicio:'10/10/2022', fecha_fin:'12/10/2022' , activo:true},
+    {id:2, usuario:{ id:10, nombre: 'Alexandre Romañon' }, usuario_suplente:{ id:2, nombre: 'Maria Ramirez' }, fecha_inicio:'1/4/2022', fecha_fin:'17/4/2022', activo:true },
+  ];
+  displayedColumns: string[] = ['usuario', 'usuario_suplente', 'fecha_inicio', 'fecha_fin', 'activo'];
+  
+  
+  constructor(
+    private matDialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
+    this.listarSuplencias();
   }
+
+
+  async listarSuplencias() {
+    console.log("listarSuplencias");
+  }
+  openAgregarSuplencia(): void {
+    let dialogRef = this.matDialog.open(CrearSuplenciaComponent, {
+      disableClose: true,
+      width:'400px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.listarSuplencias();
+      console.log("return function process");
+    });
+
+
+  }
+
+  async toggleSuplenciaEstado(element: any) {
+    console.log("objeto a inactivar" + JSON.stringify(element));
+    let mensaje:string;
+    if(element.activo){
+      mensaje = "¿Desea habilitar la suplencia?";
+    }else{
+      mensaje = "¿Desea deshabilitar la suplencia?";
+    }
+    element.mensaje = mensaje;
+
+    const dialogRef3 = this.matDialog.open( ConfirmDialogComponent, {
+      disableClose: true,
+      width:"400px",
+      data:element
+    });
+
+    dialogRef3.afterClosed().subscribe(result => {
+      if(result==='CONFIRM_DLG_YES'){
+        console.log("ACTUALIZAR EL ACTIVO");
+       
+      }
+      this.listarSuplencias();
+    });
+
+  }
+
+  async openEditarSuplencia(form: any) {
+    console.log("al editar usuario: " + JSON.stringify(form));
+    let dialogRef = this.matDialog.open(EditarSuplenciaComponent, {
+      disableClose: true,
+      data: form
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("ACTUALIZÓ");
+
+      this.listarSuplencias();
+    });
+
+  }
+
+
 
 }
