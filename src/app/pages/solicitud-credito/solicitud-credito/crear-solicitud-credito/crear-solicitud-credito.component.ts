2d42557cd5bae3b7ebc7e855e3ca10347d77beef
ian <ianmaguina@gmail.com>
2022-04-08 19:32:48 -0500

    Dato cliente - tipo cliente

--- src/app/pages/solicitud-credito/solicitud-credito/crear-solicitud-credito/crear-solicitud-credito.component.ts
+++ src/app/pages/solicitud-credito/solicitud-credito/crear-solicitud-credito/crear-solicitud-credito.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { StepperOrientation } from '@angular/cdk/stepper';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { FormValidatorService } from 'src/app/services/form-validator.service';
 
 @Component({
   selector: 'app-crear-solicitud-credito',
@@ -12,25 +13,55 @@ import { map } from 'rxjs/operators';
   ]
 })
 export class CrearSolicitudCreditoComponent {
-  isEditable = false;
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-  thirdFormGroup = this._formBuilder.group({
-    thirdCtrl: ['', Validators.required],
-  });
-  
+/* to settings constantes */
+radioGrupo: number =1;
+radioConsorcio: number =2;
+radioEmpresa: number =3;
 
-  stepperOrientation: Observable<StepperOrientation>;
+/* end */
+
+cliente:number=1;
+
+  firstFormGroup:FormGroup;
+  secondFormGroup:FormGroup;
+  thirdFormGroup:FormGroup;
+  fourthFormGroup:FormGroup;
+
+  
+  ClientSelectorControl = new FormControl('auto');
+
+  //stepperOrientation: Observable<StepperOrientation>;
   constructor(
     private _formBuilder: FormBuilder, 
-    breakpointObserver: BreakpointObserver
+    private formValidatorService: FormValidatorService,
+    /* breakpointObserver: BreakpointObserver */
     ) {
-    this.stepperOrientation = breakpointObserver
+      this.firstFormGroup = this._formBuilder.group({
+        clientSelector: [this.cliente,this.ClientSelectorControl, Validators.required],
+        nombreGrupo: [''],
+        rucGrupo: [''],
+        razonSocialEmpresa: [''],
+        rucEmpresa: [''],
+        razonSocialConsorcio: [''],
+        rucConsorcio: [''],
+      });
+      this.secondFormGroup = this._formBuilder.group({
+        secondCtrl: ['', Validators.required],
+      });
+      this.thirdFormGroup = this._formBuilder.group({
+        thirdCtrl: ['', Validators.required],
+      });
+      this.fourthFormGroup = this._formBuilder.group({
+        fourthCtrl: ['', Validators.required],
+      });
+    /* this.stepperOrientation = breakpointObserver
       .observe('(min-width: 800px)')
-      .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
+      .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical'))); */
+  }
+  guardarSeccionInformacion(element:any){
+
+  }
+  dosomething(element:any){
+    console.log(JSON.stringify(element));
   }
 }
