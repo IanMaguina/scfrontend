da0cb5f378d9dedc635bdfa7411a41cd0582cd2f
ian <ianmaguina@gmail.com>
2022-04-18 18:05:11 -0500

    formularios cliente

--- src/app/pages/solicitud-credito/solicitud-credito/datos-cliente-sc/empresas-relacionadas-sc/empresas-relacionadas-sc.component.ts
+++ src/app/pages/solicitud-credito/solicitud-credito/datos-cliente-sc/empresas-relacionadas-sc/empresas-relacionadas-sc.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormValidatorService } from 'src/app/services/form-validator.service';
 
 @Component({
   selector: 'app-empresas-relacionadas-sc',
@@ -8,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpresasRelacionadasScComponent implements OnInit {
 
-  constructor() { }
+  formulary: FormGroup;
+  displayedColumns:string[]= [
+   
+    'ruc',
+    'razon_social',
+    'id'
+  ]
+  listadoEmpresasRelacionadas:any[]=[
+    {
+      id: 1,
+      ruc: '65432189745',
+      razon_social: 'Cliente 1',
+    },
+  ];
+
+  formErrors = {
+    'ruc': '',
+    'razon_social': '',
+   
+  }
+  validationMessages = {
+    'ruc': {
+      'required': 'el ruc es requerido.'
+    },
+    'razon_social': {
+      'required': 'el razon_social es requerido.'
+    },
+   
+    
+  };
+  //Submitted form
+  submitted = false;
+  carga: boolean = false;
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _formValidatorService:FormValidatorService
+  ) { 
+    this.formulary = this._formBuilder.group({
+      ruc: ['', Validators.required],
+      razon_social: ['', Validators.required],
+    })
+    this.formulary.valueChanges.subscribe(() => {
+      this.formErrors = this._formValidatorService.handleFormChanges(this.formulary, this.formErrors, this.validationMessages, this.submitted);
+    })
+  }
 
   ngOnInit(): void {
+    console.log("ngOnInit Ref. Comerciales");
+  }
+  agregarEmpresaRelacionada(form:any){
+    console.log("agregarReferencia"+JSON.stringify(form));
+  }
+  eliminarEmpresaRelacionada(idReferencia:number){
+    console.log("eliminarReferencia"+idReferencia);
   }
 
 }
