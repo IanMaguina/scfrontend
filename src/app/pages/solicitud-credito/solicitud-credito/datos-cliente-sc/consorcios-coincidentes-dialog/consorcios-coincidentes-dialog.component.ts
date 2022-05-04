0acd56bdc3944a86a8407407bf374d2d8eaa53aa
rafaelsaenz <ar.saenz@gmail.com>
2022-05-04 08:11:31 -0500

    fix

--- src/app/pages/solicitud-credito/solicitud-credito/datos-cliente-sc/consorcios-coincidentes-dialog/consorcios-coincidentes-dialog.component.ts
+++ src/app/pages/solicitud-credito/solicitud-credito/datos-cliente-sc/consorcios-coincidentes-dialog/consorcios-coincidentes-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ClienteAgrupacion } from 'src/app/models/cliente-agrupacion.interface';
+import { SolicitudService } from 'src/app/services/solicitud.service';
+import { GlobalSettings } from 'src/app/shared/settings';
 
 @Component({
   selector: 'app-consorcios-coincidentes-dialog',
@@ -8,60 +11,43 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   ]
 })
 export class ConsorciosCoincidentesDialogComponent implements OnInit {
-  listaConsorcios:any[] = [
-    {
-      id:1,
-      razon_social:'consorcio 1 SAC',
-      empresas: [
-        {
-          id:1,
-          razon_social:'Empresa 1 SAC'
-        },
-        {
-          id:2,
-          razon_social:'Empresa 2 SAC'
-        },
-        {
-          id:3,
-          razon_social:'Empresa 3 SAC'
-        },
-      ]
-    },
-    {
-      id:1,
-      razon_social:'consorcio 2 SAC',
-      empresas: [
-        {
-          id:4,
-          razon_social:'Empresa 4 SAC'
-        },
-        {
-          id:5,
-          razon_social:'Empresa 5 SAC'
-        },
-        {
-          id:6,
-          razon_social:'Empresa 6 SAC'
-        },
-      ]
-    },
-  ];
-  nodata:boolean= false;
+  listaConsorcios: ClienteAgrupacion[] = [];
+  nodata: boolean = false;
+
+  ESTADO_SOLICITUD_EN_SOLICITANTE=GlobalSettings.ESTADO_SOLICITUD_EN_SOLICITANTE;
+  ROL_SOLICITANTE=GlobalSettings.ROL_SOLICITANTE;
+
+  clienteCodigoSapConsorcio:string;
+  rucConsorcio:string;
   constructor(
     public dialogRef: MatDialogRef<ConsorciosCoincidentesDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private solicitudService:SolicitudService    
   ) {
     //this.listaConsorcios = data.payload;
-
+    this.clienteCodigoSapConsorcio=data.clienteCodigoSapConsorcio;
+    this.rucConsorcio=data.rucConsorcio;
   }
 
   ngOnInit(): void {
+    this.listarConsorcios();
   }
-  verConsorcio(consorcio:number){
+
+  async listarConsorcios(){
+    this.solicitudService.listarConsorcioxNumeroDocumentoxFiltros({numero_documento:this.rucConsorcio}).then((data)=>{
+      console.log("Listado de Consorcios -->"+JSON.stringify(data.payload))
+      this.listaConsorcios=data.payload;
+      if (data.payload.length===0){
+        this.nodata=true;
+      }
+    })
+  }
+
+  verConsorcio(consorcio: number) {
     this.cerrarDialog(consorcio);
   }
 
-  cerrarDialog(consorcio:number){
+  cerrarDialog(consorcio: number) {
     this.dialogRef.close(consorcio);
   }
 
