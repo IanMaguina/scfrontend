711b7992461a6a62308174f1840c6ffe90464c1a
ian <ianmaguina@gmail.com>
2022-06-13 17:28:50 -0500

    lista

--- src/app/pages/solicitud-credito/solicitud-credito/datos-planes-sc/datos-planes-sc.component.ts
+++ src/app/pages/solicitud-credito/solicitud-credito/datos-planes-sc/datos-planes-sc.component.ts
@@ -1,7 +1,33 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { PlanService } from '@services/plan.service';
+import { ClienteAgrupacion } from 'src/app/models/cliente-agrupacion.interface';
+import { Empresa } from 'src/app/models/empresa.interface';
+import { Solicitud } from 'src/app/models/solicitud.interface';
+import { Zonal } from 'src/app/models/zonal.interface';
 import { DlgNuevoPlanScComponent } from './dlg-nuevo-plan-sc/dlg-nuevo-plan-sc.component';
+
+export interface EmpresaPlan {
+  id?: number,
+  id_solicitud?: number,
+  solicitud?: Solicitud,
+  id_cliente_agrupacion?: number,
+  cliente_agrupacion?: ClienteAgrupacion,
+  id_empresa?: number,
+  empresa?: Empresa,
+  zonal_codigo_sap?: string,
+  zonal?: Zonal,
+  correo?: string,
+  nombre_cliente_agrupacion?: string,
+  id_documento_identidad_cliente_agrupacion?: number,
+  numero_documento_cliente_agrupacion?: string,
+  id_documento_identidad?: number,
+  numero_documento?: string,
+  razon_social?: string,
+  telefono?: string,
+  sustento_comercial?: string,
+  participacion?: number
+}
 
 @Component({
   selector: 'app-datos-planes-sc',
@@ -9,35 +35,42 @@ import { DlgNuevoPlanScComponent } from './dlg-nuevo-plan-sc/dlg-nuevo-plan-sc.c
   styles: [
   ]
 })
+
 export class DatosPlanesScComponent implements OnInit {
-  /* @Output() onSecondFormGroup: EventEmitter<any> = new EventEmitter();
-  secondFormGroup:FormGroup; */
-  @Input() id_solicitud_editar: number;
-  constructor( 
-    private matDialog: MatDialog,
-  ){
  
+  @Input() id_solicitud_editar: number;
+  empresaPlan: EmpresaPlan[];
+  
+  constructor(
+    private matDialog: MatDialog,
+    private planService: PlanService,
+  ) {
+
   }
   ngOnInit(): void {
+    this.listarPlan();
   }
 
-  openNuevoCredito(){
+  openNuevoCredito() {
     const dialogRef = this.matDialog.open(DlgNuevoPlanScComponent, {
       disableClose: true,
-      width:"750px",
-      data:this.id_solicitud_editar
+      width: "750px",
+      data: this.id_solicitud_editar
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      this.listarRiesgos();
-      if(result === 'CONFIRM_DLG_YES'){
-        
+
+      if (result === 'CONFIRM_DLG_YES') {
+
         console.log("se agregó el plan correctamente");
       }
     });
   }
-  listarRiesgos(){
-    console.log("actualizar  componente de lista de riesgos");
+
+  listarPlan() {
+    this.planService.listarPlanEmpresa(this.id_solicitud_editar).then(data => {
+      this.empresaPlan = data.payload;
+    })
   }
 
 }
