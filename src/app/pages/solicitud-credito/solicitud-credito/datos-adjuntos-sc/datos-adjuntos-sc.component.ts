cf3e892d923d61a562aa3f0e0fe79978f1c1907a
ian <ianmaguina@gmail.com>
2022-06-15 02:19:43 -0500

    dialog

--- src/app/pages/solicitud-credito/solicitud-credito/datos-adjuntos-sc/datos-adjuntos-sc.component.ts
+++ src/app/pages/solicitud-credito/solicitud-credito/datos-adjuntos-sc/datos-adjuntos-sc.component.ts
@@ -1,7 +1,10 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { SolicitudLineaCreditoService } from '@services/solicitud-linea-credito.service';
+import { SuccessDialogComponent } from 'src/app/shared/success-dialog/success-dialog.component';
 
 
 @Component({
@@ -17,6 +20,8 @@ export class DatosAdjuntosScComponent implements OnInit {
 
   constructor(
     private _formBuilder: FormBuilder,
+    private matDialog: MatDialog,
+    private router: Router,
     private solicitudLineaCreditoService: SolicitudLineaCreditoService
   ) {
     this.fourthFormGroup = this._formBuilder.group({
@@ -29,9 +34,30 @@ export class DatosAdjuntosScComponent implements OnInit {
 
   public enviarRevision() {
     this.solicitudLineaCreditoService.enviarRevision(this.id_solicitud_editar)
-      .then(resp => console.log(resp))
+      .then(resp => {
+        console.log(resp);
+        let data = {
+          mensaje : "Su solicitud ha sido enviada al revisor con éxito !!!",
+        }
+        this.openDialog(SuccessDialogComponent,"se envió a revisión", data);
+
+      })
       .catch(function (error: HttpErrorResponse) {
         console.log(error);
       })
   }
+
+  openDialog(componente: any, msg_exito: string, data?: any) {
+    let dialogRef = this.matDialog.open(componente, {
+      disableClose: true, 
+      data: data?data:''
+    });
+
+    dialogRef.afterClosed().subscribe(_ => {
+      console.log(msg_exito);
+      this.router.navigate(['app/solicitudcredito/bandejaMisPendiendes']);
+    });
+  }
+
+
 }
