e032669540861948cdd9e93e65b9b50e0fbcfe27
ian <ianmaguina@gmail.com>
2022-06-24 02:38:53 -0500

    tablas resumen riesgo

--- src/app/models/resumen-riesgo.interface.ts
+++ src/app/models/resumen-riesgo.interface.ts
@@ -10,16 +10,21 @@ export interface ResumenRiesgo {
     linea_credito_solicitada?: string;
     linea_credito_solicitada_convertida?: string;
     variacion?: string;
+    linea_credito_solicitada_convertida_dino?: string;
+    linea_credito_solicitada_convertida_disac?: string;
 }
 
 
 export interface Elemento {
-    id: number;
-    nombre: string;
+    id?: number;
+    nombre?: string;
     linea_credito_actual?: string;
     linea_credito_actual_convertida?: string;
     moneda_actual?: string;
+    variacion?: string;
     linea_credito_solicitada?: string;
     moneda_solicitada?: string;
     linea_credito_solicitada_convertida?: string;
+    linea_credito_solicitada_convertida_dino?: string;
+    linea_credito_solicitada_convertida_disac?: string;
 }
