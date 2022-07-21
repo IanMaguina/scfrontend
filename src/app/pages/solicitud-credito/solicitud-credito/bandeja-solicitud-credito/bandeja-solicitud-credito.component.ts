11a12d7d662a26d967d485637d607da1f5a96852
ian <ianmaguina@gmail.com>
2022-07-21 12:16:54 -0500

    test

--- src/app/pages/solicitud-credito/solicitud-credito/bandeja-solicitud-credito/bandeja-solicitud-credito.component.ts
+++ src/app/pages/solicitud-credito/solicitud-credito/bandeja-solicitud-credito/bandeja-solicitud-credito.component.ts
@@ -137,9 +137,9 @@ export class BandejaSolicitudCreditoComponent implements OnInit {
       case this.ESTADO_SOLICITUD_EN_SOLICITANTE:
         this.router.navigate(['app/solicitudcredito/editarSolicitudCredito', element.id]);
         break;
-     /*  case this.ESTADO_SOLICITUD_DEVUELTO_POR_REVISOR:
+      case this.ESTADO_SOLICITUD_DEVUELTO_POR_REVISOR:
         this.router.navigate(['app/solicitudcredito/editarSolicitudCredito', element.id]);
-        break; */
+        break; 
       case this.ESTADO_SOLICITUD_EN_REVISION:
         //this.router.navigate(['app/solicitudcredito/editarSolicitudCredito', element.id]);
         this.router.navigate(['app/solicitudcredito/revisarSolicitudCredito', element.id]);
